6b0b966296b47c5fa4fb57c0bcef990f064aac27
Amiralgaby <gaby.theuws@gmail.com>
2022-09-12 21:50:31 +0200

    shellcode_checker

--- wiki_c/checkers/__init__.py
+++ wiki_c/checkers/__init__.py
@@ -1,9 +1,11 @@
 from .grammalect_checker import GrammalecteChecker
 from .url_checker import UrlChecker
 from .dokuwiki_checker import DokuwikiChecker
+from .shellcode_checker import ShellCodeChecker
 
 __all__ = [
     'GrammalecteChecker',
     'UrlChecker',
-    'DokuwikiChecker'
+    'DokuwikiChecker',
+    'ShellCodeChecker'
 ]
