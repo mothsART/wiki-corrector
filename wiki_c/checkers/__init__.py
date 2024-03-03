ce69b26d082c7cc5abbd7ad79cdf615007d64915
Amiralgaby <gaby.theuws@gmail.com>
2024-03-03 21:44:59 +0100

    ajout d'un checker

--- wiki_c/checkers/__init__.py
+++ wiki_c/checkers/__init__.py
@@ -3,11 +3,13 @@ from .url_checker import UrlChecker
 from .dokuwiki_checker import DokuwikiChecker
 from .shellcode_checker import ShellCodeChecker
 from .allowed_checker import NotAllowedChecker
+from .sequence_letter_checker import SequenceLetterChecker
 
 __all__ = [
     'GrammalecteChecker',
     'UrlChecker',
     'DokuwikiChecker',
     'NotAllowedChecker',
-    'ShellCodeChecker'
+    'ShellCodeChecker',
+    'SequenceLetterChecker'
 ]
